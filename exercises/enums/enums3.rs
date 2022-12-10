ddc08ef2ff8a77fd8be53bde43f989e3f2b03fc3
K M Lawrence <keith@kludge.co.uk>
2022-12-10 17:28:46 +0000

    Complete up to strings2.rs

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,9 +2,11 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
     // TODO: implement the message variant types based on their usage below
 }
 
@@ -37,6 +39,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match message {
+            Message::ChangeColor(color) => { self.change_color(color); },
+            Message::Echo(es) => { self.echo(es); },
+            Message::Move(pos) => { self.move_position(pos); },
+            Message::Quit => { self.quit(); },
+        }
         // TODO: create a match expression to process the different message variants
     }
 }
